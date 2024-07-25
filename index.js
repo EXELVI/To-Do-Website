810fa3fdcd355965be9dffeb7ee8f6050b9b9b3f
EXELVI <exelvi@outlook.com>
2024-07-25 09:41:55 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const http = require('http');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const databasePromise = MongoClient.connect(process.env.mongodb, { useNewUrlParser: true });
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer({
+    key: fs.readFileSync('localhost.key'),
+    cert: fs.readFileSync('localhost.crt')
+}, app);
+
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80') 
+});
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443')
+});
+
+process.on('unhandledRejection', error => {
+    console.error('Unhandled promise rejection:', error);
+});
+
+process.on('uncaughtException', error => {
+    console.error('Uncaught exception:', error);
+});
